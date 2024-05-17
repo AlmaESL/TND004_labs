35d08ac384c8129b58a19909faee939c5760287f
AlmaESL <91621109+AlmaESL@users.noreply.github.com>
2024-05-17 10:19:28 +0200

    Add files via upload

--- code/code4b/dsets.cpp
+++ code/code4b/dsets.cpp
@@ -28,17 +28,35 @@ void DSets::init() {
 // join sets named r and s where r != s
 // i.e. join trees with roots r and s
 void DSets::join(int r, int s) {
+    
     assert(r != s);
     assert(r >= 1 && r <= std::ssize(V) - 1);
     assert(s >= 1 && s <= std::ssize(V) - 1);
     assert(V[r] < 0);
     assert(V[s] < 0);
 
-    // simple union
-    V[r] = s;
+    //// simple union
+    //V[r] = s;
+
+
 
     // *** TODO ***
-    // Do Union by Size
+    
+    // Do Union by Size - join smaller subtree with larger subtree, based on number of vertices in each subtree 
+    //if subset r < subset s, join r with s and add number of vertices of r to that of s 
+    if (V[r] < V[s]) {
+        //update size of r with that of s
+        V[r] += V[s];   
+        //point r to s
+        V[s] = r;
+    }
+    //if s is smaller or r=s, join s to r 
+    else {
+        //update size of s with that of r 
+        V[s] += V[r]; 
+        //point s to r 
+        V[r] = s; 
+    }
 }
 
 // return name of current set for x
@@ -46,15 +64,27 @@ void DSets::join(int r, int s) {
 int DSets::find(int x) {
     assert(x >= 1 && x <= std::ssize(V) - 1);
 
-    // simple find
-    if (V[x] < 0) {
-        return x;
-    } else {
-        return find(V[x]);
-    }
+    //// simple find
+    //if (V[x] < 0) {
+    //    return x;
+    //} else {
+    //    return find(V[x]);
+    //}
 
      // *** TODO ***
     // find with path compression
+
+    //path compression = every vertex on path from x to root has its parent redirected to the root 
+    //check if x is already the root of the subset = base case (negative values indicate x is root)
+    if (V[x] < 0) {
+        //x found to be the root, x root returned 
+        return x; 
+    }
+    //continue searching for the subset root
+    else {
+        //move up the tree and update each subtree parent py pointing it to the root 
+        return V[x] = find(V[x]); 
+    }
 }
 
 // just in case ...
--- code/code4b/graph.cpp
+++ code/code4b/graph.cpp
@@ -9,6 +9,7 @@
 #include <cassert>     // assert
 #include <limits>      // std::numeric_limits
 #include <algorithm>   // std::make_heap(), std::pop_heap(), std::push_heap()
+#include <queue>       //priority_queue
 
 #include "graph.h"
 #include "dsets.h"
@@ -76,12 +77,124 @@ void Graph::mstPrim() const {
     std::vector<bool> done(size + 1, false);
 
     // *** TODO ***
+
+    //starting vertex, has distance 0 to itself 
+    int start_vertex = 1; 
+    dist[start_vertex] = 0; 
+    //mark sart vertex as visited 
+    done[start_vertex] = true; 
+    //start at start vertex
+    int v = start_vertex; 
+
+    //keep track of the summed egde weights
+    int weights_sum = 0; 
+
+    //iterate all vertecies in the graph until all have been inserted into the MST and marked as visited 
+    while (true) {
+        for (auto& e : table[v]) {
+
+            //vertex u whatever vertex edge e goes to
+            int u = e.to;
+
+            //if edges e to unvisited u have smaller weight, update the distance and path of u to that of v 
+            //and the weight of e 
+            if (!done[u] && dist[u] > e.weight) {
+
+                dist[u] = e.weight; 
+                path[u] = v; 
+            }
+        }
+
+        //initialize the smallest distance  
+        int min = std::numeric_limits<int>::max(); 
+
+        //for unvisited adjacent vertices, find the smallest distance
+        for (int i = 1; i <= size; i++) {
+
+            //if vertex isn't visited and the distance to it is smaller than current min, update current min 
+            if (!done[i] && dist[i] < min) {
+
+                //update smallest distance to that of i and assign vertex i to v -> stepping forward 
+                min = dist[i]; 
+                v = i; 
+            }
+        }
+
+        //no univisted vertices with finite distance found, we are done -> break the algorithm
+        if (min == std::numeric_limits<int>::max()) {
+            break; 
+        }
+
+        //update v as visited 
+        done[v] = true;
+        //add the distance of v to the weight sum
+        weights_sum += min;
+        //print edge as: from_vertex, to_vertex, edge_weight
+        std::cout << "(" << path[v] << ", " << v << ", " << dist[v] << ")" << std::endl;
+    }
+
+    //print the total weight sum
+    std::cout << "Total weight = " << weights_sum << std::endl; 
 }
+
+
 
 // Kruskal's minimum spanning tree algorithm
 void Graph::mstKruskal() const {
     // *** TODO ***
+
+    //create and reserve a vector for the edges of the graph, |E| = size-1 = |V|-1
+    std::vector<Edge> edges; 
+    edges.reserve(size - 1); 
+
+    //use the disjoint sets to partition the graph, size=|V|=number of vertices on the graph
+    DSets D(size); 
+
+    //keep track of the total weight sum 
+    int weights_sum = 0; 
+
+    //read edges from table and add them to the edge vector 
+    for (int v = 1; v <= size; v++) {
+        for (auto& e : table[v])
+
+            if (e.from < e.to) {
+                edges.push_back(e); 
+            }
+    }
+
+    //make a minHeap out of the edge vector, greater to sort in reverse since heap is maxHeap by default in STL 
+    //and we want a minHeap for the edges 
+    std::make_heap(edges.begin(), edges.end(), std::greater<Edge>()); 
+
+    //iterate the edges on the queue until queue is emptied = edge vector is empty 
+    while (!edges.empty()) {
+
+        //retrieve the topmost edge = smallest edge weight, then remove it from the queue and edge vector 
+        Edge current_edge = edges.front(); 
+        std::pop_heap(edges.begin(), edges.end(), std::greater<Edge>()); 
+        edges.pop_back(); 
+
+        //find the vertices on both sides of the current edge 
+        int from_vertices = D.find(current_edge.from); 
+        int to_vertices = D.find(current_edge.to);
+
+        //test that edges don't make a cycle
+        if (from_vertices != to_vertices) {
+
+            //join from DSets adds egde between to- and from vertices to MST by union
+            D.join(from_vertices, to_vertices); 
+            //add the current edge weight to the total weight sum 
+            weights_sum += current_edge.weight; 
+
+            //print the edges -> printing and format from Edge class
+            std::cout << current_edge << std::endl; 
+        }
+    }
+
+    //print total edge sum 
+    std::cout << "Total weight = " << weights_sum << std::endl;
 }
+
 
 // print graph
 void Graph::printGraph() const {
